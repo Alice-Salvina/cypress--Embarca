041d88f6dbff67af04ccc96747d401fbda55cf6a
Alice Salvina <alicereginasalvina@gmail.com>
2024-05-13 00:04:35 -0300

    Aumento de timeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   waitForAnimations: true,
+  defaultCommandTimeout: 8000,
   e2e: {
     baseUrl: 'https://www.embarca-staging.com.br',
     setupNodeEvents(on, config) {
